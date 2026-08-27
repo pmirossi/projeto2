17bf54d6778d67df9b1b701057a05d94321902e1
pmirossi <pedro.mancini.rossi@escola.pr.gov.br>
2026-08-27 09:34:11 -0300

    .

--- main.js
+++ main.js
@@ -76,37 +76,47 @@ let atual = 0;
 let perguntaAtual;
 let historiaFinal = ""
 
+
+
 function mostraPergunta(){
-    if(atual >= perguntas.length){
+    if (atual >= perguntas.length){
         mostraResultado()
-        return
+    return
     }
     perguntaAtual = perguntas[atual]
     caixaPerguntas.textContent = perguntaAtual.enunciado
-    caixaAlternativas.textContent = "";
-    mostraAlternativas()
+    caixaAlternativas.textContent = ""
+    mostraAlternativa()
 }
 
-function mostraAlternativas(){
+function mostraAlternativa(){
+
     for (const alternativa of perguntaAtual.alternativas){
+
         const botaoAlternativas = document.createElement("button")
-        botaoAlternativas.textContent = alternativa.texto
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
+        botaoAlternativas.textContent = alternativa.texto 
+        botaoAlternativas.addEventListener ("click", ()=> respostaSelecionada(alternativa))
         caixaAlternativas.appendChild(botaoAlternativas)
     }
 }
-
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacao
-    historiaFinal += afirmacoes + " "
-    atual++
-    mostraPergunta()
+        const afirmacoes = opcaoSelecionada.afirmacao
+        historiaFinal += afirmacoes + " "
+        atual++
+        mostraPergunta()
+    
+
+
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049 ..."
-    textoResultado.textContent = historiaFinal
-    caixaAlternativas.textContent = "";
+
+caixaPerguntas.textContent ="Em 2049..."
+textoResultado.textContent = historiaFinal
+caixaAlternativas.textContent = "";
+
 }
+
+
 
 mostraPergunta()
