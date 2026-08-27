9544dcb534ef95656978d46c32add3da50da2f71
pmirossi <pedro.mancini.rossi@escola.pr.gov.br>
2026-08-27 09:25:09 -0300

    .

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const caixaPrincipal = document.querySelector(".caixa-principal.")
-const caixaPrincipal = document.querySelector(".caixa-perguntas.")
-const caixaPrincipal = document.querySelector(".caixa-alternativas.")
-const caixaPrincipal = document.querySelector(".caixa-resultado.")
-const caixaPrincipal = document.querySelector(".texto-resultado.")
+const caixaPerguntas = document.querySelector(".caixa-perguntas.")
+const caixaAlternativas = document.querySelector(".caixa-alternativas.")
+const caixaResultado = document.querySelector(".caixa-resultado.")
+const textoResultado = document.querySelector(".texto-resultado.")
 
 const perguntas = [
     {
