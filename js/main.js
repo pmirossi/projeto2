bfa8c6efae45e4a5b5178a87641818395bcf6753
pmirossi <pedro.mancini.rossi@escola.pr.gov.br>
2026-09-17 09:36:20 -0300

    lç

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+import { aleatorio } from "./aleatorio.js";
+import { pergunta } from "./pergunta.js";
+
+
+
 const caixaPrincipal = document.querySelector(".caixa-principal")
 const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativas = document.querySelector(".caixa-alternativas")
